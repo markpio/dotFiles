7f12c32b9e06dca167b71e00fe1ccd65b16501cf
Mark Pio <mark.pio@gmail.com>
2014-01-31 09:32:10 -0500

    First commit

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+dir=~/dotFiles
+
+echo "Installing vim-gtk..."
+sudo apt-get install vim-gtk
+
+echo "Backing uip old vimrc file and installing symlink..."
+mv ~/.vimrc ~/.vimrc-original
+ln -s $dir/vimrc ~/.vimrc
+
+echo "Backing uip old bashrc file and installing symlink..."
+mv ~/.bashrc ~/.bashrc-original
+ln -s $dir/bashrc ~/.bashrc
+
+echo "Installing vim color scheme..."
+if [ -d "~/.vim/colors" ]; then
+    mkdir ~/.vim/colors/
+fi
+
+cp wombat256.vim ~/.vim/colors
