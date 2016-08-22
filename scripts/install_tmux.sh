a93a7841466df06a18d55a16bc81a4ceefcef4de
Mark Pio <mark.pio@gmail.com>
2016-08-22 19:32:28 -0400

    tmux script improvements.

--- scripts/install_tmux.sh
+++ scripts/install_tmux.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 sudo apt-get install tmux
+
+if [ -e ~/.tmux.conf ]; then
+    if [ ! -h ~/.tmux.conf ]; then
+        mv ~/.tmux.conf ~/.tmux.conf-original
+    fi
+fi
+
 ln -s ~/dotFiles/tmux.conf ~/.tmux.conf
