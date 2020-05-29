6aee904d71e31fc18b05fa0ffdc9cd4416a7ba18
user <user@localhost.localdomain>
2020-05-29 10:41:26 -0400

    Updates and more repos

--- fedora-package-script.py
+++ fedora-package-script.py
@@ -2,4 +2,33 @@
 
 import os
 
-os.system( "sudo dnf install rofi i3lock feh kakoune git i3 cargo" );
+packages = ["rofi",
+            "i3lock",
+            "feh",
+            "kakoune",
+            "git",
+            "i3",
+            "cargo",
+            "xbacklight",
+            "polybar",
+            "libxcb-devel",
+            "xcb-util-keysyms-devel",
+            "xcb-util-devel",
+            "xcb-util-wm-devel",
+            "xcb-util-xrm-devel",
+            "yajl-devel",
+            "libXrandr-devel",
+            "startup-notification-devel",
+            "libev-devel",
+            "xcb-util-cursor-devel",
+            "libXinerama-devel",
+            "libxkbcommon-devel",
+            "libxkbcommon-x11-devel",
+            "pcre-devel",
+            "pango-devel",
+            "git",
+            "gcc",
+            "automake"]
+
+
+os.system( "sudo dnf install {}".format(" ".join(packages)) );
