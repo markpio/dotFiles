900f00ddd5737eff19595fa50f13f14822836631
markpio <mark.pio@gmail.com>
2020-09-30 13:47:01 -0500

    Updating launch

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,5 +4,12 @@
 killall -q polybar
 
 echo "---" | tee -a /tmp/polybar.log
-polybar example >>/tmp/polybar.log 2>&1 &
+
+if type xrandr "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload example &
+    done
+else
+    polybar example >>/tmp/polybar.log 2>&1 &
+fi
 
