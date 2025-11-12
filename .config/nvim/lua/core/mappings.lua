715a901cb36d9a8d2d0f205563083536777648d3
Mark Pio <mark.pio@cyber.gc.ca>
2025-11-12 12:31:17 -0500

    Adding neovim mappings

--- .config/nvim/lua/core/mappings.lua
+++ .config/nvim/lua/core/mappings.lua
@@ -1,12 +1,15 @@
 
 vim.g.mapleader = " "
+
 vim.keymap.set( "n", "-", vim.cmd.Ex )
+vim.keymap.set( 'n', '<CR>', "<CMD>nohl<CR>", { desc = 'Clear highlighting' } )
 
 -- Telescope bindings
 local telescope_builtin = require( 'telescope.builtin' )
 
 vim.keymap.set( 'n', '<leader>fw', telescope_builtin.live_grep, { desc = 'Telescope live grep' } )
 vim.keymap.set( 'n', '<leader>ff', telescope_builtin.find_files, { desc = 'Telescope find files' } )
+vim.keymap.set( 'n', '<leader>fb', telescope_builtin.buffers, { desc = 'Telescope find files' } )
 vim.keymap.set( 'n', '<leader>fz', telescope_builtin.current_buffer_fuzzy_find, { desc = 'Telescope Fuzzy Find in current buffer' } )
 vim.keymap.set( 'n', '<leader>gs', telescope_builtin.git_status, { desc = 'Telescope git status' } )
 
